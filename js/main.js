5a94f758ab06c420c4453e51bc3f3487c35e28bf
Fernando-Arg <78446635+Fernando-Arg@users.noreply.github.com>
2021-04-29 12:09:42 -0500

    heart with animation

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const typed = new Typed('.card__parrafo', {
+    strings: ['Morenita de mi vida le amo mucho'],
+    typeSpeed:100,
+    backSpeed:60,
+    backDelay:1000,
+    showCursor:false,
+    loop: true
+} );
